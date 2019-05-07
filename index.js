eb70a18c549b5487c5cdc17f181708a473fc6901
Marcio Nogueira Ferreira Jorge <mnfjorge@gmail.com>
2019-05-07 14:27:20 -0300

    Initial commmit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  recipe = require('./recipes'),
+  app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.get('/recipes', (req, res) => {
+  res.json(recipe.list())
+})
+
+app.get('/recipes/:id', (req, res) => {
+  res.json(recipe.get(parseInt(req.params.id)))
+})
+
+app.listen(port, () => {
+  console.info(`Server listening on port ${port}`)
+})
